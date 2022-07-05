8503b75501946cc633567dd8b0fe7af480b5bc8d
juan david <jdtorres@unillanos.edu.co>
2022-07-05 15:58:51 -0500

    agreacion de prisma

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,48 @@
-const newUser = (req, res) => {};
-const editUser = (req, res) => {};
-const deleteUser = (req, res) => {};
+import prisma from '@prisma/client'
+    const {PrismaClient} = prisma
 
-export { addUser, editUser, deleteUser };
+    const Prisma = new PrismaClient()
+
+const newUser = async(req, res) => {
+    const post = await Prisma.post.createMany({
+                data: [
+                    // {nombre: 'daniel', pass: '1236'},
+                    // {nombre: 'jorge', pass: '1235'},
+                    {nombre: 'jorgechupador', pass: '1235'},
+                ]
+            })
+        console.log(post)
+        res.json(post)
+};
+const editUser = async(req, res) => {
+    const post = await Prisma.post.update({
+        where:{
+            id:6
+        },
+        data:{
+            nombre:"camilo"
+        }
+    })
+    res.json(post)
+};
+const deleteUser = async(req, res) => {
+    const post = await Prisma.post.delete({
+        where:{
+            id:7
+        }
+    })
+    res.json(post)
+};
+const getUsers = async(req, res) => {
+    const get = await Prisma.post.findMany()
+    res.json({get})
+};
+
+
+const getUser = (req, res) => {
+    
+};
+
+
+export { newUser, editUser, deleteUser, getUser, getUsers };
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import {getUsers,newUser,deleteUser,editUser} from '../controllers/userController.js';
 
 const router = express.Router();
+
+router.get('/', getUsers)
+router.get('/add',newUser)
+router.get('/delete',deleteUser)
+router.get('/edit',editUser)
+
+// http://localhost:4000/users
+
+
 
 export default router;
